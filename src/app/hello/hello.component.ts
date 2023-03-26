f2f4becee69e9635ea519dd7234ab5972d4831c4
HoangNV1 <hoangnv1@vnpt.vn>
2023-03-26 10:45:10 +0700

    @

--- src/app/hello/hello.component.ts
+++ src/app/hello/hello.component.ts
@@ -11,14 +11,23 @@ export class HelloComponent {
     age: 26
   }
 
-  obj = {
-    type: 'text',
-    value: 'something',
-    attributes: [], // thuộc type NamedNodeMap, dạng như một array
-    // … các thuộc tính, method khác
-  };
-
-  showInfo() {
-    alert('Inside Angular Component method');
-  }
+  authors = [
+    {
+      id: 1,
+      firstName: 'Flora',
+      lastName: 'Twell',
+      email: 'ftwell0@phoca.cz',
+      gender: 'Female',
+      ipAddress: '99.180.237.33',
+    },
+    {
+      id: 2,
+      firstName: 'Priscella',
+      lastName: 'Signe',
+      email: 'psigne1@berkeley.edu',
+      gender: 'Female',
+      ipAddress: '183.243.228.65',
+    },
+    // more data
+  ];
 }

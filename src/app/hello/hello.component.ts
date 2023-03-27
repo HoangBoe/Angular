fa4e147c4341a0abf0a8f1724ac216cda1c04dc3
HoangNV1 <hoangnv1@vnpt.vn>
2023-03-27 10:53:44 +0700

    @

--- src/app/hello/hello.component.ts
+++ src/app/hello/hello.component.ts
@@ -4,48 +4,13 @@ import { ToggleComponent } from '../toggle/toggle.component';
 @Component({
   selector: 'app-hello',
   templateUrl: './hello.component.html',
-  styleUrls: ['./hello.component.css']
+  styleUrls: ['./hello.component.css'],
 })
-export class HelloComponent implements OnInit{
-  @ViewChild('toggleComp') toggleComp!: ToggleComponent;
+export class HelloComponent implements OnInit {
+  questions = {
+    question1: true,
+    question2: false,
+  };
 
-
-  user = {
-    name: 'Hoang',
-    age: 26
-  }
-  checked = true;
-  authors = [
-    {
-      id: 1,
-      firstName: 'Flora',
-      lastName: 'Twell',
-      email: 'ftwell0@phoca.cz',
-      gender: 'Female',
-      ipAddress: '99.180.237.33',
-    },
-    {
-      id: 2,
-      firstName: 'Priscella',
-      lastName: 'Signe',
-      email: 'psigne1@berkeley.edu',
-      gender: 'Female',
-      ipAddress: '183.243.228.65',
-    },
-    // more data
-  ];
-
-  ngOnInit() {
-    // toggle undefind khi ở oninit
-    console.log("onInit", this.toggleComp);
-  }
-
-  ngAfterViewInit() {
-    // khi dùng viewChild thì toggleComp sẽ có value khi ở ngAfterViewInit
-    console.log("viewInit", this.toggleComp)
-  }
-
-  toggleInside() {
-    this.toggleComp.toggle();
-  }
+  ngOnInit() {}
 }
